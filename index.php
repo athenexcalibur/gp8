43c206a8da685b23ef9f2449de4882c1c11b52ec
Ucizi Mafeni <ucizi.m.mafeni@durham.ac.uk>
2016-10-25 21:19:55 +0100

    added bootstrap links to <head>

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
-<t gitgi?php
+<!DOCTYPE html>
+<?php
 /**
  * Created by PhpStorm.
- * User: yungcheeze
+ * User: Ucizi
  * Date: 25/10/16
  * Time: 20:53
  */
+?>
+<html>
+<head>
+    <title>Home Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+</html>
