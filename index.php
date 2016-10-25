1f33bb787845a5cff21892d2f927763df2da7878
Ucizi Mafeni <ucizi.m.mafeni@durham.ac.uk>
2016-10-25 23:18:27 +0100

    html wireframe for sidebar reference: https://www.youtube.com/watch?v=AOuPGmekLRM&index=10&list=PL6gx4Cwl9DGBPw1sFodruZUPheWVKchlM

--- index.php
+++ index.php
@@ -13,7 +13,43 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/sidebar.css">
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
+<body>
+
+    <div id="wrapper">
+
+        <div id="sidebar-wrapper">
+           <ul class="sidebar-nav">
+              <li><a href="#">Home</a> </li>
+               <li><a href="#">Account</a> </li>
+               <li><a href="#">New Listing</a> </li>
+               <li><a href="#">About/Contact</a> </li>
+           </ul>
+        </div>
+
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                   <div class="col-lg-12">
+                       <a href="#" class="btn btn-success" id="menu-toggle">Menu</a>
+                       <h1>Main Content here</h1>
+                   </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- JQuery Script to toggle menu -->
+    <script>
+        $("menu-toggle").click(function (e) {
+          e.preventDefault();
+            $("#wrapper").toggleClass("toggled");
+        });
+    </script>
+</body>
 </html>
