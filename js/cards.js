dcdae79c68a5d6e15ec1339773e6b0ad619fe483
Genieirock <genieirock1@yahoo.co.uk>
2017-03-13 21:06:50 +0000

    orders

--- js/cards.js
+++ js/cards.js
@@ -2,8 +2,9 @@ $(document).ready(function() {
 	$.get("ajax/card.html", function(data) {
 		$(".item-card").html(data);
 	});
-	$.get("ajax/orderCard.html", function(data) {
-		$(".order-card").html(data);
+	
+	$.get("ajax/orderCardPast.html", function(data) {
+		$(".past-order-card").html(data);
 	});
 });
 
