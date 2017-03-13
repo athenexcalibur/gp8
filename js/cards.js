d43d3c15b8011d67f0aea5ea321cdaa0485ae058
Unknown <u_maf@yahoo.com.au>
2017-03-13 16:12:56 +0000

    add orders.html
    (and auxiliary files)

--- js/cards.js
+++ js/cards.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
 	$.get("ajax/card.html", function(data) {
 		$(".item-card").html(data);
 	});
+	$.get("ajax/orderCard.html", function(data) {
+		$(".order-card").html(data);
+	});
 });
 
