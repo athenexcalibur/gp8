02833bf74ac0c28d692da365077ab907829480f5
Ucizi <u_maf@yahoo.com.au>
2017-03-18 18:33:18 +0000

    dynamically name cards based on search results json
    add title to item card

--- js/cards.js
+++ js/cards.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
+    $.get("php/search.php", function(data) {
+	posts = JSON.parse(data)
+	    console.log(posts);
 	$.get("ajax/card.html", function(data) {
-		$(".item-card").html(data);
+	    tmp = $("<out>").append(data);
+	    $(".item-card").each(function(i, obj){
+		if(i < posts.length){
+		    name =posts[i]["title"];
+		    name = name ? posts[i]["title"] : "Untitled";
+		    tmp.find("#title").html(name);
+		}
+		else{
+		    tmp.find("#title").html("EOF");
+		}
+		$(obj).html(tmp.clone());
+		$(obj).attr("id", i.toString());
+	    });
 	});
+    });
 });
 
