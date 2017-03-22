3b13863c62a1411889b7404714becc9906b69898
Ucizi <u_maf@yahoo.com.au>
2017-03-22 14:18:24 +0000

    add extra sexiness to message-thread.php
    hook up message display to backend
    
    first design draft of messageThreads
    
    
    update threadinfo card
    
    Cleaned up messages.css
    
    remove redundant class
    
    use jQuery to load threadname
    
    add findGetParameter() function
    
    remove php statement; reindent lines
    
    add oldmt.php (clone of messagethread.php)
    
    remove body onload function.
    
    cause its ugly as fuck

--- js/messages/messagethread.js
+++ js/messages/messagethread.js
@@ -1,3 +1,13 @@
+$(document).ready(function () {
+    fillMessages();
+    threadName = findGetParameter("threadname");
+    $("#threadname").html(threadName);
+    //shift card up
+    h = $("nav.navbar").css("height");
+    console.log(h);
+    $("main").css("padding-top", h);
+});
+
 $("#sendMsg").on("click", function()
 {
     $.post("php/messages.php",
@@ -19,54 +29,39 @@ function fillMessages()
     $.get(url, function(data)
     {
         var messages = data;
-        var allBoxes = "";
-        for (var i = 0; i < messages.length; i++)
+	var inPrototype = $(".in-prototype");
+	var outPrototype = $(".out-prototype");
+	console.log(inPrototype);
+	console.log(outPrototype);
+        for (var i = messages.length - 1; i >= 0; i--)
         {
-            var message = messages[i];
-            allBoxes += '<div class="message';
-            if(message["toname"] == otheruser)
-            {
-                allBoxes+= 'Sent';
-            }
-            else
-            {
-                allBoxes+= 'Recieved';
-            }
+	    var message = messages[i];
+	    if(message.fromname === otheruser){
+		content = inPrototype.clone();
+		content.css("display", "block")
+		content.find(".card-text").html(message.text);
+		$("#messageDiv").append(content);
+	    }
+	    else {
+		content = outPrototype.clone();
+		content.css("display", "block")
+		content.find(".card-text").html(message.text);
+		$("#messageDiv").append(content);
+	    }
 
-            allBoxes += ' card card-block">'
-                + '<p>'
-                + message["text"]
-                + '</p></div>'
-                + '<p class="time">' + message.time + '</p>'
         }
-        document.getElementById("messages").innerHTML = allBoxes;
     });
 
 }
 
-function testMessages()
-{
-    var otheruser = document.getElementById("threadname").innerHTML;
 
-    var messages = [{text : "hi foodiedave", toname : otheruser},{text : "its me foodiedave"}];
-    var allBoxes = "";
-    for (var i = 0; i < messages.length; i++)
-    {
-        var message = messages[i];
-        allBoxes += '<div class="message';
-        if(message["toname"] == otheruser)
-        {
-            allBoxes+= 'Sent';
-        }
-        else
-        {
-            allBoxes+= 'Recieved';
-        }
-
-        allBoxes += ' card card-block">'
-            + '<p>'
-            + message["text"]
-            + '</p></div>'
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    var items = location.search.substr(1).split("&");
+    for (var index = 0; index < items.length; index++) {
+        tmp = items[index].split("=");
+        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
     }
-    document.getElementById("messages").innerHTML = allBoxes;
+    return result;
 }
--- messagethread.php
+++ messagethread.php
@@ -37,102 +37,127 @@ if (!loginCheck())
 
 </head>
 
-<body onload="fillMessages()">
-<div class="snap-drawers">
+<body>
+  <div class="snap-drawers">
     <div class="snap-drawer snap-drawer-right elegant-color-dark">
-        <ul class="nav flex-column">
-            <div class="view overlay hm-white-slight">
-                <li class="nav-item">
-                    <a class="nav-link" href="orders.php">Orders</a>
-                    <div class="mask"></div>
-                </li>
-            </div>
-            <div class="view overlay hm-white-slight">
-                <li class="nav-item">
-                    <a class="nav-link" href="mylistings.php">Listings</a>
-                    <div class="mask"></div>
-                </li>
-            </div>
-            <div class="view overlay hm-white-slight">
-                <li class="nav-item">
-                    <a class="nav-link" href="inbox.php">Messages</a>
-                    <div class="mask"></div>
-                </li>
-            </div>
-            <div class="view overlay hm-white-slight">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Notifications</a>
-                    <div class="mask"></div>
-                </li>
-            </div>
-            <div class="view overlay hm-white-slight">
-                <li class="nav-item">
-                    <a class="nav-link" href="profile.php">Account</a>
-                    <div class="mask"></div>
-                </li>
-            </div>
-        </ul>
+      <ul class="nav flex-column">
+	<div class="view overlay hm-white-slight">
+	  <li class="nav-item">
+	    <a class="nav-link" href="orders.php">Orders</a>
+	    <div class="mask"></div>
+	  </li>
+	</div>
+	<div class="view overlay hm-white-slight">
+	  <li class="nav-item">
+	    <a class="nav-link" href="mylistings.php">Listings</a>
+	    <div class="mask"></div>
+	  </li>
+	</div>
+	<div class="view overlay hm-white-slight">
+	  <li class="nav-item">
+	    <a class="nav-link" href="inbox.php">Messages</a>
+	    <div class="mask"></div>
+	  </li>
+	</div>
+	<div class="view overlay hm-white-slight">
+	  <li class="nav-item">
+	    <a class="nav-link" href="#">Notifications</a>
+	    <div class="mask"></div>
+	  </li>
+	</div>
+	<div class="view overlay hm-white-slight">
+	  <li class="nav-item">
+	    <a class="nav-link" href="profile.php">Account</a>
+	    <div class="mask"></div>
+	  </li>
+	</div>
+      </ul>
     </div>
-</div>
+  </div>
   <div id="content" class="snap-content">
-      <div class="mask"></div>
+    <div class="mask"></div>
     <header>
       <!-- navbar -->
       <nav class="navbar navbar-dark navbar-fixed-top elegant-color-dark">
-	  <a href="inbox.php" id="ot" class="nav-link"><i class="material-icons">arrow_back</i></a>
-        <a href="/" id="open-left" class="navbar-brand">LOGO</a>
-        <ul class="nav navbar-nav pull-right">
-          <!--<li class="nav-item">-->
-          <!--<a class="nav-link">Login</a>-->
-          <!--</li>-->
-          <li class="nav-item">
-            <a href="#" id="open-right" class="nav-link"><i class="material-icons">account_circle</i></a>
-          </li>
-        </ul>
+	<a href="/" id="open-left" class="navbar-brand">LOGO</a>
+	<ul class="nav navbar-nav pull-right">
+	  <!--<li class="nav-item">-->
+	  <!--<a class="nav-link">Login</a>-->
+	  <!--</li>-->
+	  <li class="nav-item">
+	    <a href="#" id="open-right" class="nav-link"><i class="material-icons">account_circle</i></a>
+	  </li>
+	</ul>
       </nav>
       <!--/.navbar -->
-	  
+
     </header>
 
 
     <main>
-	
-	<div class="threadinfo card card-block">
-			<div class="col-sm-1">
-				<img src="avatar/test.png"/>
-			</div>
-			<div class="col-sm-11">
-				<p class="threadname" id="threadname">
-  
-				  <?php
-					if (isset($_GET["threadname"])) echo $_GET["threadname"];
-				  ?>
-  
-				</p>
-		</div>
-	</div>
-	
-    <div class="viewmessages" id="messages">
-	</div>
-	
-	<div class="writemessage card card-block" id="writemessage">
-			<div class="col-sm-11">
-			<div class="md-form">
-				<input id="message" type="text" class="md-textarea"></input>
-			</div>
-			</div>
-			<div class="col-sm-1">
-				<button type="button" id="sendMsg" class="btn btn-primary">SEND</button>
-			</div>
-	</div>
 
+    <div class="threadinfo card">
+      <div class="container-fluid">
+	<div class="row">
+	  <div class="col-xs-4">
+	    <img src="avatar/test.png" alt="" height="80" width="80">
+	  </div>
+	  <div class="col-xs-6">
+	    <div class="card-block">
+	      <h4 class="card-title" id=threadname>Curabitur gravida vestibulum imperdiet.</h4>
+	    </div>
+	  </div>
+	</div>
+      </div>
+    </div>
+
+      <div id="messageDiv" class="container-fluid">
+	<div class="row in-prototype">
+	  <div class="col-sm-5"></div>
+	  <div class="col-sm-7 ">
+	    <div class="card z-depth-0 card-primary message-card message-out">
+	      <div class="card-block">
+		<p class="card-text white-text">hi
+Traditional heading elements are designed to work best in the meat of your page
+content. When you need a heading to stand out, consider using a display
+heading—a larger, slightly more opinionated heading style.  </p>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+	<div class="row out-prototype">
+	  <div class="col-sm-7">
+	    <div class="card z-depth-0 grey lighten-2 message-card message-in">
+	      <div class="card-block">
+		<p class="card-text">hi</p>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-5">
+	  </div>
+	</div>
+      </div>
     </main>
 
+  </div>
+  <!--./snap-content-->
 
     <footer>
-
+      <!--important ids: sendMsg, message-->
+      <nav class="navbar navbar-fixed-bottom navbar-light bg-faded type-box">
+	<div class="container-fluid">
+	  <div class="row align-items-end">
+	    <div class="col-xs-12">
+	      <div class="input-group custom-input-group">
+		<textarea type="text" class="md-textarea custom-textarea"></textarea>
+		<a href="#" class="input-group-addon btn btn-primary btn-lg
+		  custom-btn">Send</a>
+	      </div>
+	  </div>
+	</div>
+      </div>
+      </nav>
     </footer>
-  </div>
 
   <!--Scripts-->
   <script src="bootstrap-material-design/js/jquery-3.1.1.min.js"></script>
@@ -140,10 +165,21 @@ if (!loginCheck())
   <script src="bootstrap-material-design/js/bootstrap.min.js"></script>
   <script src="bootstrap-material-design/js/mdb.min.js"></script>
   <script src="js/cards.js"></script>
-  
-   <script type="text/javascript" src="js/messages/messagethread.js"></script>
+
+  <script type="text/javascript" src="js/messages/messagethread.js"></script>
 
   <script type="text/javascript" src="snap/snap.min.js"></script>
   <script type="text/javascript" src="js/sidebar.js"></script>
+  <script>
+    snapper.on("open", function () {
+      $(".type-box").fadeOut();
+      console.log("yes");
+    });
+    snapper.on("close", function () {
+      $(".type-box").fadeIn();
+      console.log("yes");
+    });
+  </script>
+
   <!--/.Scripts-->
 </body>
