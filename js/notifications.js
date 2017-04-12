028cb2e22039152072b047324592c56a7ec6394c
Ucizi <u_maf@yahoo.com.au>
2017-04-12 12:09:58 +0000

    Add notifications page and hooked up to backend add notifications.php
    add notifications.js
    
    add notification card prototype
    
    fill notifications tab
    
    add keywords
    
    display if no keywords present
    
    style keywords list
    
    refactor: add fillKeywords()
    
    add handler to add keywords
    
    add #addKeywordsForm
    
    indent fix and comments
    
    bugfix: bad syntax
    
    add delete keyword handler

---
+++ js/notifications.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    //fill notifications
+    $.get("php/notifications/notifyUser.php?withTime", function(data) {
+	var notifications = JSON.parse(data);
+	$.each(notifications, function(index, notification) {
+	    var card = $(".notification-prototype").clone();
+	    card.removeClass("notification-prototype");
+	    card.find(".notification").text(notification.text);
+	    card.find(".timestamp").text(notification.time);
+	    $("#notifications").append(card);
+	});
+    });
+    //fillkeywords
+    fillKeywords();
+    //add keyword
+    //TODO validate input
+    $("#addKeywordForm").submit(function (event) {
+	event.preventDefault();
+	var form = $(this).serialize();
+	$.post("php/notifications/reservedWords.php", form,
+		function (data) {
+		    fillKeywords();
+		});
+    });
+    //delete keyword
+    $(document).on("click", '.list-group-item a', function(event) { 
+	event.preventDefault();
+	var str = $(this).prev().text();
+	$.post("php/notifications/reservedWords.php",{word: str, delete: true},
+		function (data) {
+		    fillKeywords();
+		});
+    });
+});
+
+function fillKeywords() {
+    $.get("php/notifications/reservedWords.php", function(data) {
+	$("#keywords.list-group").html("");
+	var keywords = JSON.parse(data);
+	$.each(keywords, function (index, keyword) {
+	    var listItem = $(".keyword-prototype").clone();
+	    listItem.removeClass("keyword-prototype");
+	    listItem.find(".text").text(keyword);
+	    $("#keywords.list-group").append(listItem);
+	});
+	if (keywords.length == 0) {
+	    var listItem = $(".keyword-prototype").clone();
+	    listItem.removeClass("keyword-prototype");
+	    listItem.find(".text").text("NO KEYWORDS");
+	    listItem.find(".text").addClass("text-muted");
+	    listItem.find("a").remove();
+	    $("#keywords.list-group").append(listItem);
+	}
+    });
+}
